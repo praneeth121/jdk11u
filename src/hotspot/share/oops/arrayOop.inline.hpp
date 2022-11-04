64955b61a933e2821431b64d54c9f4a924672f80
tiendatngcs <tiendat.ng.cs@gmail.com>
2022-11-03 21:24:31 -0500

    quick fix 3

--- src/hotspot/share/oops/arrayOop.inline.hpp
+++ src/hotspot/share/oops/arrayOop.inline.hpp
@@ -30,10 +30,25 @@
 
 void* arrayOopDesc::base(BasicType type) const {
   oop resolved_obj = Access<>::resolve(as_oop());
+  if (is_remote_oop()) tty->print_cr("!!!!!!!!! arrayOopDesc::base");
   return arrayOop(resolved_obj)->base_raw(type);
 }
 
 void* arrayOopDesc::base_raw(BasicType type) const {
+  if (is_remote_oop()) {
+    RemoteMem* r_mem = Universe::heap()->remote_mem();
+    assert(r_mem, "remote mem must be init");
+    if (r_mem->is_in_evac_set((void*)this)) {
+      void* buffering_addr = r_mem->get_corresponding_evac_buffer_address((void*)this);
+      assert(buffering_addr, "must not be NULL");
+      oop buffering_oop = oop(buffering_addr);
+      tty->print_cr("array base_raw: Remote obj %p, buffering at %p, klass %p", (void*)this, buffering_addr, buffering_oop->klass_or_null_local());
+      return reinterpret_cast<void*>(cast_from_oop<intptr_t>(buffering_oop) + base_offset_in_bytes(type));
+    }
+    assert(false, "We are not here yet");
+    return NULL;
+  }
+  if (is_remote_oop()) tty->print_cr("!!!!!!!!! arrayOopDesc::base_raw");
   return reinterpret_cast<void*>(cast_from_oop<intptr_t>(as_oop()) + base_offset_in_bytes(type));
 }
 
@@ -43,9 +58,10 @@ void arrayOopDesc::set_length(HeapWord* mem, int length) {
     assert(r_mem, "remote mem must be init");
     if (r_mem->is_in_evac_set((void*)mem)) {
       void* buffering_addr = r_mem->get_corresponding_evac_buffer_address((void*)mem);
+      assert(buffering_addr, "must not be NULL");
       oop buffering_oop = oop(buffering_addr);
       tty->print_cr("array set_length: Remote obj %p, buffering at %p, klass %p", (void*)mem, buffering_addr, buffering_oop->klass_or_null_local());
-      arrayOopDesc::set_length((HeapWord*)buffering_addr, length);
+      *(int*)(((char*)buffering_addr) + length_offset_in_bytes()) = length;
       return;
     }
     assert(false, "We are not here yet");
@@ -53,6 +69,19 @@ void arrayOopDesc::set_length(HeapWord* mem, int length) {
     return;
   }
   *(int*)(((char*)mem) + length_offset_in_bytes()) = length;
+}
+
+int arrayOopDesc::length() const {
+  if (is_remote_oop()) {
+    RemoteMem* r_mem = Universe::heap()->remote_mem();
+    if (r_mem->is_in_evac_set((void*)this)) {
+      void* buffering_addr = r_mem->get_corresponding_evac_buffer_address((void*)this);
+      assert(buffering_addr, "remote addr must not be null");
+      return *(int*)(((intptr_t)buffering_addr) + length_offset_in_bytes());
+    }
+    assert(false, "Should not reach here yet");
+  }
+  return *(int*)(((intptr_t)this) + length_offset_in_bytes());
 }
 
 #endif // SHARE_OOPS_ARRAYOOP_INLINE_HPP
